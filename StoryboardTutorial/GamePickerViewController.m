9652b30bb30b959aa65492fc7145826fbae25a5f
Hannah Carney <hannahcarneyart@gmail.com>
2015-06-09 14:11:49 +0100

    added checkmarks

--- StoryboardTutorial/GamePickerViewController.m
+++ StoryboardTutorial/GamePickerViewController.m
@@ -13,9 +13,9 @@
 @end
 
 @implementation GamePickerViewController
-
 {
     NSArray *_games;
+    NSUInteger _selectedIndex;
 }
 
 - (void)viewDidLoad
@@ -28,6 +28,8 @@
                @"Spin the Bottle",
                @"Texas Hold'em Poker",
                @"Tic-Tac-Toe"];
+    
+    _selectedIndex = [_games indexOfObject:self.game];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,7 +53,34 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GameCell"];
     cell.textLabel.text = _games[indexPath.row];
+    
+    if (indexPath.row == _selectedIndex) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if (_selectedIndex != NSNotFound) {
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:
+                                 [NSIndexPath indexPathForRow:_selectedIndex inSection:0]];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    _selectedIndex = indexPath.row;
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    
+    NSString *game = _games[indexPath.row];
+    [self.delegate gamePickerViewController:self didSelectGame:game];
 }
 
 /*
